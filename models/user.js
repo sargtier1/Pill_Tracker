fb22475a280759bebe279ae70f6858cdf2b2fb35
Eric Nguyen <enguyenusaf93@gmail.com>
2018-12-04 21:01:29 -0600

    adding code to user.js

---
+++ models/user.js
@@ -0,0 +1,36 @@
+module.exports = function (sequelize, DataTypes) {
+
+  var User = sequelize.define('user', {
+    id: {
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    firstname: {
+      type: DataTypes.STRING,
+      notEmpty: true
+    },
+    lastname: {
+      type: DataTypes.STRING,
+      notEmpty: true
+    },
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: true
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    status: {
+      type: DataTypes.ENUM('active', 'inactive'),
+      defaultValue: 'active'
+    }
+
+  });
+
+  return User;
+
+}
