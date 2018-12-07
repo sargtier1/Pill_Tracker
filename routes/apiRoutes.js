c5269be38519cab75633872d84ab7d066e889dde
Julio Sanchez <jsanchez191980802@gmail.com>
2018-12-06 20:16:11 -0600

    added Joi to package JSON and added verificaiton to post ApiRoute

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,5 @@
 var db = require("../models");
+var Joi = require('joi');
 
 module.exports = function(app) {
   // Get all examples
@@ -10,6 +11,15 @@ module.exports = function(app) {
 
   // Create a new example
   app.post("/api/examples", function(req, res) {
+    var schema = Joi.object().keys({
+      username: Joi.string().alphanum().min(3).max(30).required(),
+      password: Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/),
+      email: Joi.string().email({ minDomainAtoms: 2 }).required(),
+    });
+
+    var result = Joi.validate({ username: 'abc', email: 'email@email.com' }, schema);
+    
+
     db.Example.create(req.body).then(function(dbExample) {
       res.json(dbExample);
     });
