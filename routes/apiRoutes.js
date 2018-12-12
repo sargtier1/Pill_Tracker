d1e9ccc55315299767512816a91a5f12e4d9e09f
Julio Sanchez <jsanchez191980802@gmail.com>
2018-12-11 18:52:56 -0600

    added space between buttons

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -14,24 +14,9 @@ module.exports = function(app) {
    });
 
   // Create a new example
-<<<<<<< HEAD
-  app.post("/api/examples", function(req, res) {
-    var schema = Joi.object().keys({
-      username: Joi.string().alphanum().min(3).max(30).required(),
-      password: Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/),
-      email: Joi.string().email({ minDomainAtoms: 2 }).required(),
-    });
-
-    var result = Joi.validate({ username: 'abc', email: 'email@email.com' }, schema);
-    
-
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-=======
   app.post("/api/new/medicine", function(req, res) {
     db.medications.create(req.body).then(function(dbMedication) {
       res.json(dbMedication);
->>>>>>> 68d67b73f9d51926bb6a101fd309b45b74bde4c0
     });
   });
 
