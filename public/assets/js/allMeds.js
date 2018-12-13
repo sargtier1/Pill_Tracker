ceb021d905acb8918732c0f85a2dbe725e3901f8
Julio Sanchez <jsanchez191980802@gmail.com>
2018-12-12 18:22:12 -0600

    all info displays on user home.  Need to format it better now

--- public/assets/js/allMeds.js
+++ public/assets/js/allMeds.js
@@ -1,3 +1,23 @@
 $.get('api/all', function(data){
     console.log(data);
-})
+    // For each book that our server sends us back
+  for (var i = 0; i < data.length; i++) {
+    // Create a parent div to hold book data
+    var wellSection = $("<div>");
+    // Add a class to this div: 'well'
+    wellSection.addClass('row');
+    wellSection.addClass('col-md-3');
+    // Add an id to the well to mark which well it is
+    wellSection.attr("id", "med-well-" + i);
+    // Append the well to the well section
+    $("#med-section").append(wellSection);
+
+    // Now  we add our book data to the well we just placed on the page
+    //$("#book-well-" + i).append("<h5>" + (i + 1) + ". " + "</h5>");
+    $("#med-well-" + i).append("<h5>Name: " + data[i].drugName + "</h5>");
+    $("#med-well-" + i).append("<h5>Start Time: " + data[i].startTime + "</h5>");
+    $("#med-well-" + i).append("<h5>Frequency: " + data[i].frequency + "</h5>");
+    $("#med-well-" + i).append("<h5>Notes: " + data[i].notes + "</h5>");
+  }
+    
+ })
